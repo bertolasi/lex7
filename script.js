887db804f2d9d2f2cf8b7b8fafed2415607a198d
bertolasi <62144734+bertolasi@users.noreply.github.com>
2023-11-28 08:45:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Este é um exemplo simples de script JavaScript
 console.log('Script carregado com sucesso!');
+
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'flex' : 'none';
+}
