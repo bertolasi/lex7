3d8f8033fb712a0e540727ae1b40bd532af38e41
bertolasi <62144734+bertolasi@users.noreply.github.com>
2025-07-01 01:58:03 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Este é um exemplo simples de script JavaScript
-console.log('Script carregado com sucesso!');
-
-function toggleMenu() {
-    var menu = document.querySelector('.menu');
-    menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'flex' : 'none';
-}
